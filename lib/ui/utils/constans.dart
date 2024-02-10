949f186736a38923ca3c4d0bf31fc39623ba21d3
1223355566 <abdomohamedfoly@gmail.com>
2024-02-11 01:36:40 +0200

    initial commet

---
+++ lib/ui/utils/constans.dart
@@ -0,0 +1,5 @@
+abstract class Constants{
+static const String baseUrl="";
+static const String internetErrorMassage="No internet connection please make sure you are connected";
+static const String errorMassage="Some Thing went Rong";
+}
